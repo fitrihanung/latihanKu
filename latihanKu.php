7e7a9b90425974ce21fe414f2849e4c0df839f78
fitri hanung w <f3z0n3@gmail.com>
2015-12-15 13:54:27 +0700

    Commit Pertama

---
+++ latihanKu.php
@@ -0,0 +1,33 @@
+<?php
+$nama ="fitri hanung wibowo";
+
+if ($nama = 1) {
+	echo "yes,namanya benar fitri hanung wibowo";
+} else {
+	echo "maaf,namanya bukan fitri hanung wibowo";
+}
+
+
+for ($i=0; $i < 10; $i++) { 
+	echo"<br>". $i . "<br>";
+}
+
+$name = "fitri hanung wibowo";
+switch ($name) {
+	case 'fitri':
+		echo "namanya kurang lengkap...";
+		break;
+	case 'hanung':
+		echo "yang ini namanya juga kurang lengkap...";
+		break;
+	case 'wibowo':
+		echo "yang ini juga kurang lengkap juga namanya...";
+		break;
+	case 'fitri hanung wibowo':
+		echo "nah, kalo ini baru lengkap namanya FITRI HANUNG WIBOWO";
+		break;
+	default:
+		echo "salah semua";
+		break;
+}
+?>
